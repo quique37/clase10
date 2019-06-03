8b89f5111b7eea37d3b5807113283fee07e55714
Enrique Jimenez <enrique.jmz37@gmail.com>
2019-06-03 17:35:11 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+function suma(a, b) {
+	return a + b;
+}
+module.exports.suma = suma;
+
+function resta(a, b) {
+	return a - b;
+}
+module.exports.resta = resta;
+
+function multiplicacion(a, b) {
+	return a * b;
+}
+module.exports.multiplicacion = multiplicacion;
+
+function division(a, b) {
+	return a / b;
+}
+module.exports.division = division;
