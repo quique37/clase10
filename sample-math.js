35555af8b7307ab1847059589ef509529b7d5970
Enrique Jimenez <enrique.jmz37@gmail.com>
2019-06-03 17:35:41 -0500

    Add files via upload

---
+++ sample-math.js
@@ -0,0 +1,5 @@
+const customMath = require('./custom-math')
+console.log(customMath.suma(4, 6 ));
+console.log(customMath.resta(15, 6));
+console.log(customMath.multiplicacion(3, 7));
+console.log(customMath.division(25, 5));
